cc3500f1526833caf689931103cb0dda6f6cc7b1
Shaundai Person <shaundaiperson@gmail.com>
2019-12-19 23:28:02 -0500

    create app using create react app

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Pouches from './Pouches';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <Pouches />
+      </ul>
+      Hi
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Pouches.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Pouches(){
+        return (
+            <li>
+                <h2>Item Name</h2>
+                <h3>Description</h3>
+                <p>Box Number-clickable link</p>
+                <p>Quantity</p>
+            </li>
+        )
+}
+
+export default Pouches;
