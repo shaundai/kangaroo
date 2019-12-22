bf80e0c8d2e759215d333bfe4469d86b776f1086
Shaundai Person <shaundaiperson@gmail.com>
2019-12-21 23:11:28 -0500

    add a little css

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,12 +4,17 @@ import Pouches from './Pouches';
 import Item from './Item';
 
 function App() {
-  const [itemList, setItemList] = useState({ name: "scissors", desc: "the scissors", box: 3, location: "here", owner: "salle", quantity: 4 })
+  const [itemList, setItemList] = useState([{ name: "scissors", desc: "the scissors", box: 3, location: "here", owner: "sally", quantity: 4 },  {name: "brush", desc: "the brush", box: 3, location: "here", owner: "sally", quantity: 1 }])
+
+  function addItem(){
+    let newItemList = [...itemList];
+    setItemList(newItemList);
+}
   return (
     <div className="App">
         <Pouches />
         <ul>
-        {Object.keys(itemList).map(key => <Item key={key} details={itemList} />)}
+        {Object.keys(itemList).map(key => <Item key={key} details={itemList[key]} />)}
         </ul>
     </div>
   );
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Item(props) {
     const { name, desc, quantity, box, location, owner } = props.details; //destructuring like a bawse
         return (
-            <li>
+            <li className="item">
                 <h2>{name}</h2>
                 <h3>Description: {desc}</h3>
                 <p>Box Number: clickable link {box}</p>
